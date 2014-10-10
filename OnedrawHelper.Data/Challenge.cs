f8785c625463d1b584b6f0ab4b0d96caf694fd48
paralleltree <paralleltree@outlook.com>
2014-10-10 17:21:52 +0900

    それぞれのテーマと個々の開催を表すクラスを追加

---
+++ OnedrawHelper.Data/Challenge.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnedrawHelper.Data
+{
+    public class Challenge
+    {
+        public DateTime StartTime { get; set; }
+        public IEnumerable<string> Subject { get; set; }
+        public IEnumerable<string> Rules { get; set; }
+    }
+}
---
+++ OnedrawHelper.Data/Theme.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnedrawHelper.Data
+{
+    public class Theme
+    {
+        public string Name { get; set; }
+        public Challenge NextChallenge { get; set; }
+        public string SourceScreenName { get; set; }
+        public string ExtractionPattern { get; set; }
+        public string HashTag { get; set; }
+    }
+}
